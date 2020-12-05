c9b6f18076313f24e6cfeae230d713f8cc2c6625
Rodrigo Webler <rwebler@gmail.com>
2020-12-05 16:57:27 -0300

    WIth random map

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
 mod map;
+mod map_builder;
+mod player;
 
 pub mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::build(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+        }
     }
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 fn main() -> BError {
