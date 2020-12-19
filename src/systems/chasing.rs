720ee504d7de12962379fbdb6c965d66115c9f57
Rodrigo Webler <rwebler@gmail.com>
2020-12-19 15:08:13 -0300

    FoV

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -4,12 +4,13 @@ use crate::prelude::*;
 #[read_component(ChasingPlayer)]
 #[read_component(Health)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn chasing(
     #[resource] map: &Map,
     ecs: &SubWorld,
     commands: &mut CommandBuffer
 ) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
+    let mut movers = <(Entity, &Point, &ChasingPlayer, &FieldOfView)>::query();
     let mut positions = <(Entity, &Point, &Health)>::query();
     let mut player = <(&Point, &Player)>::query();
     let player_pos = player.iter(ecs).nth(0).unwrap().0;
@@ -22,7 +23,10 @@ pub fn chasing(
         map,
         1024.0
     );
-    movers.iter(ecs).for_each(|(entity, pos, _)| {
+    movers.iter(ecs).for_each(|(entity, pos, _, fov)| {
+        if !fov.visible_tiles.contains(&player_pos) {
+            return;
+        }
         let idx = map_idx(pos.x, pos.y);
         if let Some(destination) = DijkstraMap::find_lowest_exit(
             &dijkstra_map, idx, map
